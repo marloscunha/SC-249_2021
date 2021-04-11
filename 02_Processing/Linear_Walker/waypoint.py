47b201664341ed6233fae408e66e2de45b966665
marloscunha <66571402+marloscunha@users.noreply.github.com>
2021-04-10 23:54:07 -0300

    Flags for "useNoise" and "useAcc"
    Flags have been added to "waypoint.py" to enable/disable the use of the acceleration in the calculations and to enable/disable the use of acceleration noise.

--- 02_Processing/Linear_Walker/waypoint.py
+++ 02_Processing/Linear_Walker/waypoint.py
@@ -3,7 +3,6 @@ This class shall handle with creation of Waypoints that will be followed by the 
 
 Still have to include:
 - Linear walker with constant speed
-- Mouse-click waypoint follower.
 
 '''
 
@@ -22,6 +21,10 @@ class waypoint():
         self.currWP = PVector(random(1) * self.window_wSize, random(1) * self.window_hSize)
         
     def generateCommands(self, walkerPos, walkerSpd, walkerAcc):
+        
+        flag_useAcc = 0
+        flag_useNoise = 0
+        
         deltaPath = PVector.sub(self.currWP, walkerPos)        
         if deltaPath.mag() == 0:
             self.generateTarget()
@@ -30,11 +33,18 @@ class waypoint():
         desSpd = deltaPath.copy().limit(self.maxSpd) # Desired Speed
         
         steering = PVector.sub(desSpd,walkerSpd).limit(self.maxForce)
-        #randomNoise = PVector.random2D().mult(0.14)
-        randomNoise = PVector(0,0)
+        
+        if flag_useNoise == 0:
+            randomNoise = PVector(0,0)
+        else:
+            randomNoise = PVector.random2D().mult(0.14)
+        
         
         outAcc = PVector.add(PVector.add(walkerAcc, steering), randomNoise)
         outSpd = PVector.add(walkerSpd, outAcc).limit(self.maxSpd)
-        outPos = PVector.add(walkerPos, outSpd)
+        if flag_useAcc == 1:
+            outPos = PVector.add(walkerPos, desSpd)
+        else:
+            outPos = PVector.add(walkerPos, outSpd)
         
         return outAcc, outSpd, outPos
