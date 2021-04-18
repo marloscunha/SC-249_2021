1ae6812f44f04ea69a1af2d03ed10aa28620c02f
marloscunha <66571402+marloscunha@users.noreply.github.com>
2021-04-18 18:38:40 -0300

    Simulation with wind
    Wind has been added to the simulation.

--- 02_Processing/LAB02/LAB02.pyde
+++ 02_Processing/LAB02/LAB02.pyde
@@ -6,17 +6,23 @@ import pdb
 
 def setup():
     size(1200, 800)
+    resolution = 10 # Amount of pixels to be used as simulation resolution.
     background(255)
     
-    global vehicle, WP, Env, flag_wind, stateEnum, subStateEnum
-    vehicle = vehicle(x0=200 , y0=200, vx_0=0, vy_0=0.0, ax_0 = 0, ay_0 = 0, printSize=6, printColor=[000, 255, 00])
-    WP = waypoint(width, height)
-    Env = env()
-    flag_wind = 0    
+    global vehicle, WP, Env, flag_wind, stateEnum, subStateEnum, windType
     
+    Env = env(resolution)
+    vehicle = vehicle(x0=200 , y0=200, vx_0=0, vy_0=0.0, ax_0 = 0, ay_0 = 0, printSize=6, printColor=[000, 255, 00])
+    flag_wind = 0 
+    
+    # Needs to be corrected.
+    WP = waypoint(width, height)    
     WP.generateTarget('first')
-    stateEnum = ["Position Hold", "Waypoint capture", "Circle Around", "8 Navigation"]
+    
+    stateEnum    = ["Position Hold", "Waypoint capture", "Circle Around", "8 Navigation"]
     subStateEnum = ["NA","Circling around C1", "Transitioning to C2", "Circling around C2", "Transitioning to C1"]
+    
+    windType = ["None", "Random values", "Perlin Noise"]
 
     
 def draw():
@@ -24,25 +30,30 @@ def draw():
     Env.updateEnv(WP, vehicle, flag_wind)
     vehicle.plotVehicle()
     
-    
-    
-    
-    
+    # Simulation status:
     fill(255,0,0)
     textSize(12)
     text("Current state      : " + str(vehicle.state) + " (" + str(stateEnum[vehicle.state])  + ")", 30, 30)
     text("Current substate : " + str(vehicle.subState)+ " (" + str(subStateEnum[vehicle.subState])  + ")", 30, 42)
     text("Target position  : " + str(vehicle.tgtAngPos), 30, 54)
     text("Distance to target: " + str(vehicle.tgtDist), 30, 66)
+    text("Wind: " + str(windType[Env.windFlow.status]), 30, 78)
     
     fill(255)
             
 def mouseClicked():
-    global flag_wind
     if mouseButton == LEFT:
         WP.setWPtype(1) # Random
         WP.updateTarget(-1, PVector(mouseX,mouseY))
     elif mouseButton == RIGHT:
         WP.setWPtype(2) # Eight Figure
         WP.updateTarget(-1, PVector(mouseX,mouseY))
-    flag_wind *= -1
+    
+def keyPressed():
+    global flag_wind
+    if key == 'w':
+        if flag_wind == 2:
+            flag_wind = 0
+        else:
+            flag_wind +=1
+        Env.activateWind(flag_wind)
--- 02_Processing/LAB02/env.py
+++ 02_Processing/LAB02/env.py
@@ -1,80 +1,74 @@
+import pdb
+
 class FlowField():
     def __init__(self, Resolution):
         
          # Resolution of grid relative to window width and height in pixels
-        self.resolution = resolution
+        self.resolution = Resolution
         
         # Grid information
-        self.cols = width/Resolution
-        self.rows = height/Resolution
+        self.rows = int(height/Resolution)
+        self.cols = int(width/Resolution)
         
         #Vector that will store the field components:
-        field = [[PVector(0,0)]*cols]*rows
+        self.field = [[PVector(0,0)]*self.cols]*self.rows
+        
+        #Flow Status
+        self.status = 0
         
     # Sets up the flow field
     # fieldID = 0: stationary flow field.
     # fieldID = 1: random flow field.
     # fieldID = 3: perlin noise.
-    def setField(self, fieldID):
+    def setField(self, fieldID, maxMag=1):
         xoff = 0
         yoff = 0
-        for row in len(range(self.rows)): 
-            for col in len(range(self.cols)):
-                if fieldID == 0: # No flow field
-                    field[row][col] = PVector(0,0)
+        self.status = fieldID
 
-                elif fieldID == 1: # Random values from 0 to 10.
-                    field[row][col] = PVector.mult(PVector.random2D(),10)
+        for row in range(self.rows): 
+            for col in range(self.cols):
+                if fieldID == 0: # No flow field
+                    self.field[row][col] = PVector(0,0)
+
+                elif fieldID == 1: # Random 
+                    self.field[row][col] = PVector.mult(PVector.random2D(),maxMag)
                 
                 elif fieldID == 2: #Perlin noise
                     theta = map(noise(xoff,yoff), 0, 1, 0, TWO_PI)
-                    field[row][col] = PVector(cos(theta), sin(theta))
+                    self.field[row][col] = PVector(cos(theta), sin(theta))
                     yoff += 0.1
             if fieldID == 2:
                 xoff += 0.1
                 yoff = 0
     
     # Returns the field value at a given position:
-    def getField(self, Pose):
-        col = constrain(Pose.x/self.resolution, 0, self.cols-1)
-        row = constrain(Pose.x/self.resolution, 0, self.rows-1)
-        
+    def getField(self, Pose=PVector(0,0)):
+        col = int(constrain(Pose.x/self.resolution, 0, self.cols-1))
+        row = int(constrain(Pose.y/self.resolution, 0, self.rows-1))
+
         return self.field[row][col].copy()
 
 class env():
-
-    def __init__(self):
-        self.windVec = PVector(0,0)
+    def __init__(self, resolution=10):
+        self.envResolution = 10 # Resolution set to 10 pixels.
+        self.windFlow = FlowField(self.envResolution)
     
-    def wind(self, flag_wind):
-        if flag_wind == 1:
-            self.windVec.set(-0.5,0.5)
-        elif flag_wind == -1:
-            self.windVec.set(-1,1)
-    
-    def generateForces(self):
-        
-        windForce = self.windVec
-        return windForce
-    
-    
+    def activateWind(self, flag_wind):
+        self.windFlow.setField(flag_wind)        
             
-    def updateEnv(self, WP, walker, flag_wind=0):
-        windForce = self.generateForces()
-        #print(windForce, flag_wind)
-        
-        # Inner forces and commands:    
-        InnLoop_outAcc, InnLoop_outSpd, InnLoop_outPos, distTarget_mag = walker.generateCommands(WP)
+    def updateEnv(self, WP, vehicle, flag_wind=0):
+        # Vehicle's inner forces and commands:    
+        InnLoop_outAcc, InnLoop_outSpd, InnLoop_outPos, distTarget_mag = vehicle.generateCommands(WP)
         
         WP.updateTarget(distTarget_mag)
         
         # Outer forces and commands:
-        outerAcc, outerSpd, outerPos = PVector(0,0), PVector(0,0), windForce
+        outerAcc, outerSpd, outerPos = PVector(0,0), PVector(0,0), self.windFlow.getField(vehicle.Pos)
         
         # Overall effect:
         outAcc = InnLoop_outAcc + outerAcc
         outSpd = InnLoop_outSpd + outerSpd
         outPos = InnLoop_outPos + outerPos
             
-        walker.updatePose(outAcc, outSpd, outPos)
+        vehicle.updatePose(outAcc, outSpd, outPos)
         
